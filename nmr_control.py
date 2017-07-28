381331a9895d8e3a7b91d2e13d4737792a3d6167
gcassella <gino.cassella@stfc.ac.uk>
2017-07-28 10:44:00 +0100

    Added series acquisition and fitting.
    Changes to single FID fitting:
      - Initial phase diff. is now calculated from the gradient of the first milisecond of the signal (this causes small inaccuracies when there is a rapid variation artifact at the beginning of the signal, but this does not affect the quality of fit significantly and these cases are rare). This allows the SciPy curve_fit algorithm to cope with phase diffs of PI without manual specification.
    
    Series acquisition capability:
      - Acquisition interval and number of FIDs to acquire can be specified
      - Tested to run for 48 acquisitions in 20 minute interval with no issues

--- nmr_control.py
+++ nmr_control.py
@@ -31,6 +31,7 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setup_override()
 
         self.data = []
+        self.series_amplitudes = []
 
         # Single FID Acquisition Signals
 
@@ -47,19 +48,23 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Single FID Fitting Signals
 
-        self.fit_button.clicked.connect(self.fit_fid)
+        self.fit_button.clicked.connect(lambda: self.fit_fid(plot=True))
 
-        # FID Series Acquisition Signals
+        # FID Series Signals
 
         self.fid_dir = ""
-        self.save_directory_button.clicked.connect(self.set_fid_dir)
+        self.fid_directory_button.clicked.connect(self.set_fid_dir)
         self.afid_start_button.clicked.connect(self.get_fid_series)
+        self.fid_directory_button_2.clicked.connect(self.set_fid_dir)
+        self.plot_fid_series_button.clicked.connect(self.fit_fid_series)
+        self.export_fid_series_button.clicked.connect(self.export_fid_series_fit)
 
         self.MAX_RATE = 1000000
 
     def set_fid_dir(self):
         fn = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        self.save_directory_display.setText(fn)
+        self.fid_directory_display.setText(fn)
+        self.fid_directory_display_2.setText(fn)
         self.fid_dir = fn
 
     def get_fid_series(self):
@@ -67,6 +72,7 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
         # go out of scope after the succesful execution of get_fid_series
         # same with i
         self.timer = QtCore.QTimer()
+        self.timer_timer = QtCore.QTimer()
 
         self.i=0
         def get_and_save():
@@ -81,11 +87,44 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.timer.stop()
 
             self.statusbar.showMessage('{} of {} series acquired'.format(self.i,self.afid_n_series_spin.value()))
+
+        def update_progress():
+            self.fid_series_acq_progress.setValue((self.fid_series_acq_progress.value()+1)%100)
             
         self.timer.timeout.connect(get_and_save)
-        self.timer.start(self.afid_sampling_period_spin.value()*6*1e3)
+        self.timer_timer.timeout.connect(update_progress)
+        self.timer.start(self.afid_sampling_period_spin.value()*60*1e3)
+        self.timer_timer.start(self.afid_sampling_period_spin.value()*600)
 
+    def fit_fid_series(self):
+        self.series_amplitudes = []
+        self.i = 0
+        self.series_timescale = []
+        while self.i < self.afid_n_series_spin_2.value():
 
+            if self.import_fid(filename=self.fid_dir + '/' + str(self.i)):
+                self.series_amplitudes.append(self.fit_fid(plot=False)[0])
+                self.series_timescale.append(self.afid_sampling_period_spin_2.value()*self.i)
+            self.i+=1
+
+        self.fid_series_fitting_plot.plot_figure(self.series_timescale,self.series_amplitudes,format='r.')
+
+    def export_fid_series_fit(self):
+        if len(self.series_amplitudes) == 0:
+            self.statusbar.showMessage('No FID data collected')
+            return
+
+        options = QFileDialog.Options()
+        filename, _ = QFileDialog.getSaveFileName(self,
+                                                    "QFileDialog.getSaveFileName()",
+                                                    "",
+                                                    "All Files (*)",
+                                                    options=options)
+        
+        if filename:
+            to_write = np.column_stack((self.series_timescale,self.series_amplitudes))
+            np.savetxt(filename, to_write)
+    
     def setup_override(self):
         # Override QtDesigner compiled settings to create QPlots
         # This function literally exists because I'm too lazy to set up
@@ -118,7 +157,16 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
         self.fid_fitting_plot.setSizePolicy(sizePolicy)
         self.fid_fitting_plot.setObjectName("fid_fitting_plot")
 
-    def fit_fid(self):
+        self.fid_series_fitting_plot = QPlot(self.series_fitting_tab,xlabel="Time / min",ylabel=r"V_pp")
+        self.fid_series_fitting_plot.setGeometry(QtCore.QRect(210, 10, 541, 431))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.fid_series_fitting_plot.sizePolicy().hasHeightForWidth())
+        self.fid_series_fitting_plot.setSizePolicy(sizePolicy)
+        self.fid_series_fitting_plot.setObjectName("fid_series_fitting_plot")
+
+    def fit_fid(self, plot=True):
         if len(self.data) == 0:
             self.statusbar.showMessage('No FID data collected')
             return
@@ -127,25 +175,30 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
             lower_bounds = [self.vpp_bound_lower.value(),
                             self.decay_time_bound_lower.value(),
                             self.frequency_bound_lower.value(),
-                            self.phase_diff_bound_lower.value(),
+                            0,
                             self.constant_bound_lower.value()]
 
             upper_bounds = [self.vpp_bound_upper.value(),
                             self.decay_time_bound_upper.value(),
                             self.frequency_bound_upper.value(),
-                            self.phase_diff_bound_upper.value(),
+                            6.28,
                             self.constant_bound_upper.value()]
 
             bounds = (lower_bounds,upper_bounds)
         else:
             bounds = (-np.inf,np.inf)
-        
+
         if self.initial_checkbox.isChecked():
             p0 = [self.vpp_bound_initial.value(),
                   self.decay_time_bound_initial.value(),
                   self.frequency_bound_initial.value(),
-                  self.phase_diff_bound_initial.value(),
+                  0,
                   self.constant_bound_initial.value()]
+
+            # Supply initial phase based on knowledge of signal
+
+            if np.sign(np.gradient(self.data[:1000])[0]) == -1:
+                p0[3] = np.pi
 
         # Standard SciPy curve fitting, see documentation at
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
@@ -158,16 +211,18 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
             self.statusbar.showMessage('Inappropriate initial or boundary values')
             return
 
-        self.fid_fitting_plot.axes.cla()
-        self.fid_fitting_plot.axes.plot(timescale, exp_dec(timescale,*popt),'r-')
-        self.fid_fitting_plot.axes.plot(timescale, self.data,'b-')
-        self.fid_fitting_plot.draw()
+        if plot == True:
+            self.fid_fitting_plot.axes.cla()
+            self.fid_fitting_plot.axes.plot(timescale, exp_dec(timescale,*popt),'b-')
+            self.fid_fitting_plot.axes.plot(timescale, self.data,'r-')
+            self.fid_fitting_plot.draw()
 
-        self.vpp_found.setText(str(popt[0]*1e3) + ' mV')
-        self.decay_constant_found.setText(str(popt[1]*1e3) + ' ms')
-        self.frequency_found.setText(str(popt[2]) + ' Hz')
-        self.phase_diff_found.setText(str(popt[3]) + ' rad')
-        self.constant_found.setText(str(popt[4]))
+            self.vpp_found.setText(str(popt[0]*1e3) + ' mV')
+            self.decay_constant_found.setText(str(popt[1]*1e3) + ' ms')
+            self.frequency_found.setText(str(popt[2]) + ' Hz')
+            self.constant_found.setText(str(popt[4]))
+
+        return popt
 
     def export_fid(self, filename=None):
         if not filename:
@@ -187,17 +242,22 @@ class NMRControl(QtWidgets.QMainWindow, Ui_MainWindow):
             to_write = np.column_stack((timescale,self.data))
             np.savetxt(filename, to_write)
 
-    def import_fid(self):
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self,
-                                                  "QFileDialog.getOpenFileName()",
-                                                  "",
-                                                  "All Files (*)",
-                                                  options=options)
+    def import_fid(self, filename=None):
+        if not filename:
+            options = QFileDialog.Options()
+            filename, _ = QFileDialog.getOpenFileName(self,
+                                                    "QFileDialog.getOpenFileName()",
+                                                    "",
+                                                    "All Files (*)",
+                                                    options=options)
 
         if filename:
-            self.data = list(zip(*np.loadtxt(filename)))[1]
-            self.plot_return_pulse()
+            try:
+                self.data = list(zip(*np.loadtxt(filename)))[1]
+                self.plot_return_pulse()
+                return True
+            except FileNotFoundError:
+                return False
 
     def send_pulse(self, pulse_frequency, pulse_duration, pulse_density, pulse_amplitude, return_pulse_duration):
         # Ensure sampling rate is below the maximum rate for the current DAQ, 1MHz
